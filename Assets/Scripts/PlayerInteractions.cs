8d3328cb2708f0fec39253bf0a2f2359ac94b7e2
B0xito <zarystabof@gmail.com>
2023-11-06 11:23:15 -0300

    v 1.6.5

--- Assets/Scripts/PlayerInteractions.cs
+++ Assets/Scripts/PlayerInteractions.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class PlayerInteractions : MonoBehaviour
 {
+    #region Movement Variables
     [SerializeField] float speed = 8;
     [SerializeField] float rotSpeed = 200;
+    #endregion
+
+    #region Stamina
+    float staminaMax = 50f;
+    float staminaReduction = 5f;
+    float staminaIncrement = 5f;
+    #endregion
+
     private void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
     {
         float h = Input.GetAxisRaw("Horizontal") * rotSpeed * Time.deltaTime;
         float v = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
         transform.Translate(0, 0, v);
         transform.Rotate(h, 0, 0);
+
+        if (Input.GetKey(KeyCode.F))
+        {
+            staminaMax -= staminaReduction * Time.deltaTime;
+        }
+        else
+        {
+            staminaMax += staminaIncrement * Time.deltaTime;
+        }
     }
+
+
 }
